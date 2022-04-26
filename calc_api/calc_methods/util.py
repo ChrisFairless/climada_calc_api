5c4a4b5d4afa1202bc3d6e7eee12e5d9aa5b38bc
Chris Fairless <chrisfairless@hotmail.com>
2022-04-26 18:33:17 +0100

    Better error handling for country lookups

--- calc_api/calc_methods/util.py
+++ calc_api/calc_methods/util.py
@@ -1,6 +1,8 @@
 from climada.util.coordinates import country_to_iso
+from celery import shared_task
 
 
+@shared_task
 def country_iso_from_parameters(location_scale, location_code, location_poly, representation="alpha3"):
     """
     Decode location parameters to country codes to pass to the API
@@ -20,22 +22,27 @@ def country_iso_from_parameters(location_scale, location_code, location_poly, re
     if location_poly:
         raise ValueError("API doesn't handle polygon queries yet")
 
-    if location_scale:
-        if location_scale == "global":
-            raise ValueError("API doesn't handle global queries yet")  # TODO
-        if location_code:
-            if location_scale in ['ISO3', 'admin0', 'country']:
-                country_iso3alpha = country_to_iso(location_code, representation)
-            elif location_scale == "admin1":
-                raise ValueError("API doesn't handle admin1 data yet")  # TODO
-            elif location_scale == "admin2":
-                raise ValueError("API doesn't handle admin2 data yet")  # TODO
-            else:
-                raise ValueError("location_scale parameter must be one of 'ISO3', 'admin0', 'admin1', 'admin2'")
-    else:
+    if not location_code:
+        raise ValueError("API requires location_code data (for now)")  # TODO
+    if not location_scale:
         raise ValueError("API requires location_scale data (for now)")  # TODO
+
+    if location_scale == "global":
+        raise ValueError("API doesn't handle global queries yet")  # TODO
+    elif location_scale in ['ISO3', 'admin0', 'country']:
+        country_iso3alpha = country_to_iso(location_code, representation)
+    elif location_scale == "admin1":
+        raise ValueError("API doesn't handle admin1 data yet")  # TODO
+    elif location_scale == "admin2":
+        raise ValueError("API doesn't handle admin2 data yet")  # TODO
+    else:
+        raise ValueError("location_scale parameter must be one of 'ISO3', 'admin0', 'admin1', 'admin2'")
 
     if not isinstance(country_iso3alpha, list):
         country_iso3alpha = [country_iso3alpha]
 
-    return country_iso3alpha
+    if len(country_iso3alpha) > 1:
+        raise ValueError("Can't handle multiple countries yet")
+
+    # TODO make this able to handle lists!!
+    return country_iso3alpha[0]
