491f563cacd1a2e6bbf9585eac81c6f90ea9e3b8
Chris Fairless <chrisfairless@hotmail.com>
2022-04-28 07:48:57 +0100

    Remove sample data creation script

--- setup.sh
+++ setup.sh
@@ -3,5 +3,4 @@ python manage.py makemigrations calc_api --noinput
 python manage.py migrate calc_api --noinput
 python manage.py makemigrations django_celery_results --noinput
 python manage.py migrate django_celery_results --noinput
-python manage.py generate_sample_data
 python manage.py generate_measure_data
