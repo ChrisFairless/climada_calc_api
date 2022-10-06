e4e25d3245a889bab15d98692778b930f772fb85
Chris Fairless <chrisfairless@hotmail.com>
2022-10-06 09:53:18 +0200

    Try removing migrations from setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python manage.py makemigrations calc_api --noinput
-python manage.py migrate calc_api --noinput
-python manage.py makemigrations django_celery_results --noinput
-python manage.py migrate django_celery_results --noinput
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
+# python manage.py makemigrations django_celery_results --noinput
+# python manage.py migrate django_celery_results --noinput
 python manage.py generate_sample_data
 python manage.py generate_measure_data
 python manage.py collectstatic --noinput
