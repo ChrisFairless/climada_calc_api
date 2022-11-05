eeaf16f0203063a19649210953ae0ba3a36906e1
Chris Fairless <chrisfairless@hotmail.com>
2022-11-05 15:47:31 +0100

    Update testing script

--- calc_api/vizz/test/test_ninja.py
+++ calc_api/vizz/test/test_ninja.py
@@ -5,13 +5,36 @@ import logging
 import time
 import importlib
 
+# ---------------
+
+# This is a script that systematically runs through all possible endpoint queries and makes a
+# request for each of them. If they aren't in the database, and the server is able to calculate
+# new data, it will set off a calculation.
+#
+# If new data are being calculated, you'll need to run this a second time to collect and log the
+# results for future use.
+#
+# It's terrible for testing because it doesn't always tell you what's going wrong unless you're
+# testing against a server that can't set off asynchronous calculations (recommended before deploy).
+#
+# Note to future developers: write tests. It would have saved so much time.
+
+# ---------------
+
 LOGGER = logging.getLogger(__name__)
 
 server_address = 'http://0.0.0.0:8000/'
-# server_address = 'https://reca-api.herokuapp.com/'
+#server_address = 'https://reca-api.herokuapp.com/'
+#server_address = 'https://reca-v1-app-pfvsg.ondigitalocean.app/'
 
-location_list = ['Saint Kitts and Nevis', 'Port-au-Prince', 'Havana, Cuba']
-#location_list = ['Havana, Cuba']
+wait_between_requests = 0
+#measure_id_list = [143, 145]
+measure_id_list = [2, 4]
+
+
+location_list = ['Jamaica', 'Saint Kitts and Nevis', 'Port-au-Prince', 'Havana, Cuba']
+#location_list = ['Toamasina', 'Mozambique', 'Manila, Philippines']
+# location_list = ['Jamaica']
 
 calculation_endpoints = {
     # 'rest/vizz/map/hazard/climate': 'MapHazardClimateRequest',
@@ -22,10 +45,10 @@ calculation_endpoints = {
     # 'rest/vizz/timeline/hazard': 'TimelineHazardRequest',
     # 'rest/vizz/timeline/exposure': 'TimelineExposureRequest',
     # 'rest/vizz/timeline/impact': 'TimelineImpactRequest',
-    # 'rest/vizz/widgets/biodiversity': 'BiodiversityWidgetRequest',
     'rest/vizz/widgets/cost-benefit': 'CostBenefitWidgetRequest',
-    'rest/vizz/widgets/social-vulnerability': 'SocialVulnerabilityWidgetRequest',
     'rest/vizz/widgets/risk-timeline': 'TimelineWidgetRequest',
+    'rest/vizz/widgets/social-vulnerability': 'SocialVulnerabilityWidgetRequest',
+    # 'rest/vizz/widgets/biodiversity': 'BiodiversityWidgetRequest',
     # 'rest/vizz/exceedance/hazard': 'ExceedanceHazardRequest',
     # 'rest/vizz/exceedance/impact': 'ExceedanceImpactRequest',
 }
@@ -55,7 +78,7 @@ class TestEndpoints(unittest.TestCase):
 
     def test_geocode_endpoint(self):
         LOGGER.debug("Testing geocoding endpoint")
-        url = server_address + 'rest/vizz/geocode/autocomplete?query=Miami'
+        url = server_address + 'rest/vizz/geocode/autocomplete?query=Port-au-Prince'
         response = requests.request("GET", url, headers={}, data={})
         self.assertEqual(response.status_code, 200)
 
@@ -64,36 +87,38 @@ class TestEndpoints(unittest.TestCase):
             endpoint,
             location_name,
             haz_name,
-            exposure_type,
             impact_type,
+            measure_list,
             year,
             climate_scenario,
             return_period,
-            aggregation_scale,
-            aggregation_method,
+            # aggregation_scale,
+            # aggregation_method,
             units_hazard,
             units_exposure):
-        key = endpoint + ' ' + haz_name + ' ' + str(year) + ' ' + \
-              climate_scenario + ' ' + return_period
+
+        key = f'{location_name} {endpoint} {haz_name} {impact_type} {year}, ' \
+              f'{climate_scenario} {return_period} {measure_list}'
+        LOGGER.info(key)
 
         request_settings = {
             'hazard_type': haz_name,
             'hazard_event_name': None,
             'hazard_rp': return_period,
-            'exposure_type': exposure_type,
             'impact_type': impact_type,
             'scenario_name': climate_scenario,
             'scenario_climate': None,  # Test these later
             'scenario_growth': None,
             'scenario_year': year,
-            'measure': [12],
+            'measure_ids': measure_list,
             # 'location_scale': 'country',
             'location_code': None,
             'location_poly': None,
             'location_name': location_name,
-            'aggregation_scale': aggregation_scale,
-            'aggregation_method': aggregation_method,
+            # 'aggregation_scale': aggregation_scale,
+            # 'aggregation_method': aggregation_method,
             'format': 'tif',
+            'units_currency': 'dollars',
             'units_hazard': units_hazard,
             'units_warming': 'fahrenheit',
             'units_exposure': units_exposure,
@@ -105,6 +130,8 @@ class TestEndpoints(unittest.TestCase):
 
         url = server_address + endpoint
         response = requests.post(url, headers={}, json=req)
+        LOGGER.info(response.json()['job_id'])
+
         return key, response
 
     def test_all_calculation_endpoints(self):
@@ -121,7 +148,7 @@ class TestEndpoints(unittest.TestCase):
                     # LOGGER.warning(f"Skipping map endpoints for now: {endpoint}")
                     # continue
                 else:
-                    aggregation_scale = 'country'
+                    aggregation_scale = 'all'
                 for haz_name, haz_options in options.items():
                     if haz_name == "extreme_heat":
                         LOGGER.warning("Skipping extreme heat for now")
@@ -141,6 +168,11 @@ class TestEndpoints(unittest.TestCase):
                                 'units': 'dollars'
                             },
                             {
+                                'exposure_type': 'economic_assets',
+                                'impact_type': 'assets_affected',
+                                'units': 'dollars'
+                            },
+                            {
                                 'exposure_type': 'people',
                                 'impact_type': 'people_affected',
                                 'units': 'people'
@@ -149,27 +181,46 @@ class TestEndpoints(unittest.TestCase):
                     else:
                         raise ValueError('haz_name must be extreme_heat or tropical_cyclone')
 
-                    year_options = [yr['value'] for yr in haz_options['scenario_options']['year']['choices']]
+                    year_options = [yr['value'] for yr in haz_options['scenario_options']['year']['choices'] if yr['value']!=2020]
                     scenario_options = [scen['value'] for scen in haz_options['scenario_options']['climate_scenario']['choices']]
                     rp_options = [rp['value'] for rp in haz_options['scenario_options']['return_period']['choices']]
-                    if 'impact' not in endpoint:
-                        rp_options = [rp for rp in rp_options if rp != "aai"]
+                    if endpoint in ['rest/vizz/widgets/social-vulnerability', 'rest/vizz/widgets/biodiversity']:
+                        year_options = [year_options[0]]
+                        scenario_options = [scenario_options[0]]
+                        rp_options = [rp_options[0]]
+                        exposures = [exposures[0]]
                     aggregation_method = 'max' if 'hazard' in endpoint else 'sum'
+
                     for exp in exposures:
+                        # TODO when there are two hazards this will have to be smarter
+                        if endpoint == 'rest/vizz/widgets/cost-benefit':
+                            rp_options = ['aai']
+                            if exp['impact_type'] in ['assets_affected', 'economic_impact']:
+                                measure_options = [[m_id] for m_id in measure_id_list]
+                            else:
+                                continue
+                        else:
+                            measure_options = [None]
+
                         for year in year_options:
                             for climate_scenario in scenario_options:
-                                if climate_scenario == 'historical' and 'timeline' in endpoint:
-                                    continue
-                                if climate_scenario == scenario_options[1]:
-                                    rp_list = rp_options
-                                else:
-                                    rp_list = [rp_options[-1]]
+                                # if climate_scenario == 'historical' and 'timeline' in endpoint:
+                                #     continue
+                                #if climate_scenario == scenario_options[1]:
+                                rp_list = rp_options
+                                #else:
+                                #    rp_list = [rp_options[-1]]
+
                                 for return_period in rp_list:
-                                    key, response = self.submit_job(endpoint, location_name, haz_name,
-                                                                    exp['exposure_type'], exp['impact_type'], year,
-                                                                    climate_scenario, return_period, aggregation_scale,
-                                                                    aggregation_method, haz_unit, exp['units'])
-                                    job_details = f"""
+                                    for measure_list in measure_options:
+                                        key, response = self.submit_job(endpoint, location_name,
+                                                                        haz_name,
+                                                                        exp['impact_type'],
+                                                                        measure_list,
+                                                                        year, climate_scenario, return_period,
+                                                                        # aggregation_scale, aggregation_method,
+                                                                        haz_unit, exp['units'])
+                                        job_details = f"""
         Endpoint:\n
         {endpoint}\n
         Request:\n
@@ -177,59 +228,68 @@ class TestEndpoints(unittest.TestCase):
         Response:\n
         {response.json()}
                                     """
-                                    LOGGER.debug(job_details)
-                                    if response.status_code != 200:
-                                        LOGGER.warning(f'Job submission had trouble: {response}')
-                                    self.assertEqual(response.status_code, 200)
+                                        LOGGER.debug(job_details)
+                                        if response.status_code != 200:
+                                            LOGGER.warning(f'Job submission had trouble: {response}')
+                                        self.assertEqual(response.status_code, 200)
 
-                                    job_dict[key] = {'endpoint': endpoint,
-                                                     'job_id': response.json()['job_id'],
-                                                     'request': response.request.body,
-                                                     'response': response.json()}
+                                        status = response.json()['status']
+                                        if status == "SUCCESS":
+                                            LOGGER.debug("Job was a success")
+                                        else:
+                                            job_dict[key] = {'endpoint': endpoint,
+                                                             'job_id': response.json()['job_id'],
+                                                             'request': response.request.body,
+                                                             'response': response.json()}
 
-                                    value = job_dict[key]
-                                    url = server_address + value['endpoint'] + '/' + value['job_id']
-                                    status = ''
-                                    response = requests.request('GET', url, headers={})
-                                    status = response.json()['status']
-                                    if status not in ['SUCCESS', 'FAILURE', 'PENDING']:
-                                        LOGGER.warning(f"Job submission not a success: {response.json()}")
-                                #    time.sleep(30)
-                                # self.assertEqual(response.status_code, 200)
-                                # self.assertEqual(status, 'SUCCESS')
-        #                      break
-        #                  break
-        #              break
-        #          break
-        #       break
-        #    break
-        # break
+                                            value = job_dict[key]
+                                            url = server_address + value['endpoint'] + '/' + value['job_id']
+                                            response = requests.request('GET', url, headers={})
+                                            status = response.json()['status']
+                                            if status not in ['SUCCESS', 'FAILURE', 'PENDING']:
+                                                LOGGER.warning(f"Job submission not a success: {response.json()}")
+                                        if wait_between_requests > 0:
+                                            time.sleep(wait_between_requests)
+                                    # self.assertEqual(response.status_code, 200)
+                                    # self.assertEqual(status, 'SUCCESS')
+            #                             break
+            #                         break
+            #                     break
+            #                 break
+            #             break
+            #         break
+            #     break
+            # break
 
 
-        for key, value in job_dict.items():
-            url = server_address + value['endpoint'] + '/' + value['job_id']
-            response = requests.request('GET', url, headers={})
-            status = response.json()['status']
-            poll_count = 0
-            if status != 'SUCCESS':
-                while status not in ['SUCCESS', 'FAILURE'] and poll_count < 8:
-                    poll_count += 1
-                    time.sleep(10)
-                    LOGGER.info(f"...polling. Status: {status}  URL: {url}")
-                    response = requests.request('GET', url, headers={})
-                    status = response.json()['status']
-                if status != "SUCCESS":
-                    LOGGER.info(f"Job was not a success.")
-                    LOGGER.info(f"\n\nJob info: \n{response.json()}")
-                    LOGGER.info(f"\n\nJob request: \n{value}")
-            # self.assertEqual(response.status_code, 200)
-            # self.assertEqual(status, 'SUCCESS')
 
-            image_uri = response.json()['response_uri']
-            if image_uri is not None:
-                image_response = requests.request('GET', image_uri, headers={}, data={})
-                self.assertEqual(image_response.status_code, 200)
-                self.assertEqual(image_response.headers['Content-Type'], 'image/tiff')
+        # Code to run through the results. Not used right now. Instead run the whole
+        # script a second time to see if everything logged well.
+        if False:
+            for key, value in job_dict.items():
+                url = server_address + value['endpoint'] + '/' + value['job_id']
+                response = requests.request('GET', url, headers={})
+                status = response.json()['status']
+                poll_count = 0
+                if status != 'SUCCESS':
+                    while status not in ['SUCCESS', 'FAILURE'] and poll_count < 8:
+                        poll_count += 1
+                        time.sleep(10)
+                        LOGGER.info(f"...polling. Status: {status}  URL: {url}")
+                        response = requests.request('GET', url, headers={})
+                        status = response.json()['status']
+                    if status != "SUCCESS":
+                        LOGGER.info(f"Job was not a success.")
+                        LOGGER.info(f"\n\nJob info: \n{response.json()}")
+                        LOGGER.info(f"\n\nJob request: \n{value}")
+                # self.assertEqual(response.status_code, 200)
+                # self.assertEqual(status, 'SUCCESS')
+
+                image_uri = response.json()['response_uri']
+                if image_uri is not None:
+                    image_response = requests.request('GET', image_uri, headers={}, data={})
+                    self.assertEqual(image_response.status_code, 200)
+                    self.assertEqual(image_response.headers['Content-Type'], 'image/tiff')
 
 
 if __name__ == "__main__":
